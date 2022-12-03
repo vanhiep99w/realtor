94e8fc9eba65a103ad35cd9d89142efbc51a01b4
vanhiep99w <vanhiep99w@gmail.com>
2022-12-03 15:37:29 +0700

    fix: remove spinner-active class when Spinner component unmounted

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,16 +2,18 @@ import spinner from "@/assets/spinner.svg";
 import { createPortal } from "react-dom";
 import { useEffect } from "react";
 
-function Spinner({ isShow = true }) {
+function Spinner() {
   const body = document.body;
-  if (!isShow) {
-    body.classList.remove("spinner-active");
-    return null;
-  }
-
   body.classList.add("spinner-active");
 
-  // make fixed to handle the scroll issue
+  useEffect(() => {
+    // Remove spinner-active class when component unmounted
+    return () => {
+      body.classList.remove("spinner-active");
+    }
+  }, [])
+
+  // make fixed position to handle the scroll issue
   return createPortal(
     <div className="bg-black bg-opacity-30 flex items-center justify-center h-screen w-screen fixed top-0 left-0 z-20">
       <div className="w-20 h-20">
