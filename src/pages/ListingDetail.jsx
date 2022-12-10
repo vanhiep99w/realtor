6382b0929119f6f3159b8f582fe49484400d076b
vanhiep99w <vanhiep99w@gmail.com>
2022-12-10 17:28:32 +0700

    feat: add share link listing

--- src/pages/ListingDetail.jsx
+++ src/pages/ListingDetail.jsx
@@ -6,6 +6,7 @@ import Spinner from "@/components/Spinner.jsx";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/all";
 
 function ListingDetail(props) {
   const { listingId = 0 } = useParams();
@@ -23,30 +24,43 @@ function ListingDetail(props) {
     setLoadingListing(false);
   };
 
-  console.log(listing);
-
   useEffect(() => {
     fetchListing();
   }, []);
+
+  const shareLinkHandle = () => {
+    navigator.clipboard.writeText(window.location.href);
+    toast.info("Link copied", { position: "top-right" });
+  };
+
   return (
     <div>
-      <Swiper
-        modules={[Autoplay, Navigation, Pagination, EffectFade]}
-        slidesPerView={1}
-        navigation
-        pagination={{ type: "progressbar" }}
-        effect="fade"
-        autoplay={{ delay: 3000 }}
-      >
-        {listing?.imgUrls?.map((url, index) => (
-          <SwiperSlide key={url}>
-            <div
-              className="w-full overflow-hidden h-[400px]"
-              style={{ background: `url(${url}) center no-repeat` }}
-            ></div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="relative">
+        <Swiper
+          modules={[Autoplay, Navigation, Pagination, EffectFade]}
+          slidesPerView={1}
+          navigation
+          pagination={{ type: "progressbar" }}
+          effect="fade"
+          autoplay={{ delay: 3000 }}
+        >
+          {listing?.imgUrls?.map((url, index) => (
+            <SwiperSlide key={url}>
+              <div
+                className="w-full overflow-hidden h-[400px]"
+                style={{ background: `url(${url}) center no-repeat` }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div
+          className="absolute top-3 right-3 z-10 p-3 bg-white rounded-full cursor-pointer"
+          onClick={shareLinkHandle}
+        >
+          <FaShare />
+        </div>
+      </div>
+
       {loadingListing && <Spinner />}
     </div>
   );
